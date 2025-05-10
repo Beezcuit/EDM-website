375feb1309c556c9fea8b8806cfbb62458a72d6c
Ayberk Özkan <ayberkkozkan@hotmail.com>
2025-05-10 17:26:43 +0300

    Initial commit

---
+++ pulse-of-the-universe/js/vanta-hero.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    VANTA.NET({
+      el: "#vanta-hero",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      color: 0x00ffff, // Neon mavi
+      backgroundColor: 0x000000,
+      maxDistance: 18.0,
+      spacing: 16.0
+    });
+  });
+  
